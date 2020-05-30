48f120ade5dfc795cd13951f354822db4b885681
ThehellAA <48060593+ThehellAA@users.noreply.github.com>
2020-05-30 01:28:52 +0100

    Delete FormValidationException.java

--- Java Resources/com/getupenglish/forms/FormValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.getupenglish.forms;
-
-public class FormValidationException extends Exception {
-    public FormValidationException( String message ) {
-        super( message );
-    }
-}
